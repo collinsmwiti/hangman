20ebb6daa28e4f7a1a67ad47e176a02ab6e0b0f3
Collins Mwiti <collotush@gmail.com>
2017-04-21 16:09:46 +0300

    I have added functionality in Prompter.java to prevent method overloading in App.java and my first functionality has worked. I have also commented on my functionality in Prompter.java

--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -9,22 +9,25 @@ import static org.junit.Assert.*;
 //class HangmanTest
 public class HangmanTest {
     Hangman hangman ;
-    
+
     // used to add additional functionality which applies to all tasks within the test class but in a generic way
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
+    // indicates this method must be executed before each test in the class, so as to execute some pre-conditions necessary for the test
     @Before
     public void setUp() throws Exception {
         hangman = new Hangman("collins");
 
     }
 
+    //is for choosing a character and therefore the answer of the character should match to the word guessed
     @Test
     public void choosingACharacterInTheAnswerReturnsTrue() throws Exception {
         assertTrue(hangman.applyGuess('c'));
     }
 
+    // is to enhance that the character you guess is included and it matters. You cannot alter because its the rules of the game
     @Test
     public void ifAStringIsEnteredOnlyTheFirstCharacterMatters() throws Exception{
         assertTrue(hangman.applyGuess("cljn"));
